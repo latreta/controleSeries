c50b82323bbfed475258f8d5e697dfe08b9ce1da
La Treta <yves_henry13@hotmail.com>
2019-05-01 05:44:23 -0300

    exibicao de series

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SeriesController extends Controller
+{
+
+    public function index(Request $request)
+    {
+        $series = [
+            'House M.D.',
+            'Dexter',
+            'The Rookie',
+        ];
+
+        return view('series.index', compact('series'));
+    }
+
+}
