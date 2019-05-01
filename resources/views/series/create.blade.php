b0e6f8720b77129251921ae778eee25d115fc1bf
La Treta <yves_henry13@hotmail.com>
2019-05-01 15:08:37 -0300

    base sqlite e blade

---
+++ resources/views/series/create.blade.php
@@ -0,0 +1,16 @@
+@extends('layout')
+
+@section('cabecalho')
+Adicionar Série
+@endsection
+
+@section('conteudo')
+<form method="post">
+    <div class="form-group">
+        <label for="nome">Nome:</label>
+        <input type="text" class="form-control" placeholder="Nome da serie" name="nome" id="nome">
+    </div>
+    
+    <button class="btn btn-primary mt-2">Cadastrar</button>
+    </form>
+@endsection
