d48534e246da712b46738c817181730bc0e115e2
La Treta <yves_henry13@hotmail.com>
2019-05-03 17:04:09 -0300

    validacao de form request

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -1,79 +1,64 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers; 
 
 use App\Serie;
-use Illuminate\Http\Request;
+use App\Http\Requests\SeriesFormRequest;
+use Illuminate\Http\Request; 
 
-class SeriesController extends Controller
-{
-    const MENSAGEM = 'mensagem';
+class SeriesController extends Controller {
+    const MENSAGEM = 'mensagem'; 
     const MENSAGEM_ERRO = 'mensagemErro';
-
-    public function index(Request $request)
-    {
+    
+    public function index(Request $request) {
         $series = Serie::query()->orderBy('nome')->get();
         $mensagens = $this->recuperaMensagens($request);
         $mensagem = $mensagens[self::MENSAGEM];
         $mensagemErro = $mensagens[self::MENSAGEM_ERRO];
         return view('series.index', compact('series', 'mensagem', 'mensagemErro'));
     }
-
-    private function recuperaMensagens(Request $request)
-    {
+    
+    private function recuperaMensagens(Request $request) {
         $mensagem = $request->session()->get(self::MENSAGEM);
         $mensagemErro = $request->session()->get(self::MENSAGEM_ERRO);
-        // $request->session()->remove(self::MENSAGEM);
-        // $request->session()->remove(self::MENSAGEM_ERRO);
         return compact('mensagem', 'mensagemErro');
-
     }
 
-    public function create()
-    {
+    
+    public function create() {
         return view('series.create');
     }
 
-    public function store(Request $request)
-    {
+    public function store(SeriesFormRequest $request) {
         $nome = $request->nome;
-        if (strlen($nome) <= 0) {
-            $request->session()->flash(
-                self::MENSAGEM_ERRO,
-                "Ocorreu um erro ao tentar incluir."
-            );
-        } else {
-            $serie = Serie::create(
+        
+        $serie = Serie::create(
                 [
-                    'nome' => $nome,
+                    'nome' =>$nome
                 ]
             );
+            
             $status = $serie->save();
             $request->session()->flash(
                 self::MENSAGEM,
                 "Série {$serie->nome} criada com sucesso."
             );
-        }
-
+            
         return redirect()->route('listar_series');
-
     }
-
-    public function destroy(Request $request){
+    
+    public function destroy(Request $request) {
         $serie_id = $request->id;
-
-        if(!is_null($serie_id)){
+        
+        if ( ! is_null($serie_id)) {
             $status = Serie::destroy($serie_id);
-            if($status){
+            if ($status) {
                 $request->session()->flash(self::MENSAGEM, "Série removida com sucesso.");
             }
-            else{
+            else {
                 $request->session()->flash(self::MENSAGEM_ERRO, "Falha ao realizar operação.");
-
             }
         }
-
         return redirect()->route('listar_series');
-
     }
 }
